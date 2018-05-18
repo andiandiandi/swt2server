059dcc0f4d31e6611922ab242e9dbd2ea03d53a1
manjaro <manjaro@manjaro>
2018-05-18 17:20:08 +0200

    calculateStich, gespielte Karten und ob diese gültig waren sind implementiert

--- src/game/CardGame.java
+++ src/game/CardGame.java
@@ -16,11 +16,18 @@ public class CardGame {
 	private List<Player> playerList;
 	
 	private Map<Player,List<Card>> stiche;
+	private Map<Player, Card> board;
+	private Map<Player, List<Card>> assignedCards;
+	
+	private Player roundStarter;
+	
 	
 	public CardGame(List<Player> playerList) {
 		this.playerList=playerList;
-		stiche = new HashMap<Player,List<Card>>();
+		stiche = new HashMap();
 		cardGenerator = new CardGenerator();
+		board=new HashMap(4);
+		assignedCards=new HashMap();
 	}
 	
 
@@ -28,8 +35,62 @@ public class CardGame {
 		
 	}
 	
-	private Map<Player,Integer> calculateStiche(){
-		return null;
+	public boolean validPlay(Player p, Card card) {
+		//	Check card: Ob eine Trumpfkarte gespielt wurde oder das Symbol, zu dem des Spieleröffners, passt
+		if(!card.isTrumpf()	&&	card.getSymbol() !=	board.get(roundStarter).getSymbol()) {
+			
+			if(assignedCards.get(p).contains(board.get(roundStarter).getSymbol())) {
+				System.out.println("Zug nicht zulässig. Sie besitzen noch eine spielbare Karte");
+				return false;
+			}
+			else {
+				board.put(p, card);
+				return true;
+			}
+		}
+		return true;
+	}
+	
+	public void playedCard(Player p, Card card) {
+		/*
+		 * 	Regel: Spieler 1 legt seine Karte und eröffnet die Runde, die anderen Spieler müssen, dasselbe Symbol legen. 
+		 * 	Ansonsten einen Trumpf oder eine Fehlfarbe.
+		 */
+		if(board.isEmpty()) {
+			roundStarter=p;
+			board.put(p, card);			
+		}
+		else {
+			while(!validPlay(p, card)) {
+				System.out.println("Unzulässiger Zug!");
+			}
+			board.put(p, card);
+		}	
+	}
+	
+	private void calculateStiche(){
+		/*
+		 * 	Wertigkeiten: Herz T > Kreuz D > D >  B > Karo Ass > Karo T > Karo K > rest sind Fehlfarben
+		 */
+		Map.Entry<Player, Card> firstEntry = board.entrySet().iterator().next();
+		Player largestKey = firstEntry.getKey();
+		Card largestKeyValue = firstEntry.getValue();
+		
+		for(Map.Entry<Player, Card> map : board.entrySet()	) {
+			Player key = map.getKey();
+			Card value = map.getValue();
+			if(value.compareTo(largestKeyValue) > 0) {
+				largestKeyValue = value;
+				largestKey = key;
+			}
+		}
+
+		roundStarter = largestKey;
+		List<Card> wonCards=new LinkedList<Card>();
+		for(Player p: playerList) {
+				wonCards.add(board.get(p));
+		}
+		stiche.put(largestKey, wonCards);
 	}
 	
 	public void shuffle(){
